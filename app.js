202745a3e14d278448d3c4912aa44bc3c4e02d3c
yongde2900 <com414141@gmail.com>
2020-10-17 17:37:20 +0800

    feat: add login function

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Include package and define related server varibles
 const express = require('express')
 const exhdb = require('express-handlebars')
-const bodyParse = require('body-parse')
+const bodyParser = require('body-parser')
+const Users = require('./models/user')
 const port = 3000
 const app = express()
 require('./config/mongoose')
@@ -10,11 +11,27 @@ require('./config/mongoose')
 app.engine('hbs', exhdb({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
-app.use(bodyParse.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Setting routes
 app.get('/', (req, res) =>{
     res.render('index')
+})
+
+app.post('/users', (req, res) => {
+    const { email, password } = req.body
+    return Users.findOne({email:email})
+        .lean()
+        .then( user => {
+            if (user.password === password){
+                res.render('welcome', {user})
+            }else{
+                res.render('index', {wrong:'wrong'})
+            }
+        })
+        .catch(error => {
+            res.render('index', {wrong:'wrong'})
+        })
 })
 
 // Start the express sweb and listen for connection
